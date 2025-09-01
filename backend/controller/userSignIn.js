dd91f68272597ec0e20525c1efe10510a179215d
Ganesh Vishwakarma <ganeshvishwakarma02012003>
2025-09-01 08:36:59 +0530

    fixed tokenOption

--- backend/controller/userSignIn.js
+++ backend/controller/userSignIn.js
@@ -15,10 +15,9 @@ async function userSignInController(req, res) {
 
     const user = await userModel.findOne({ email });
 
-
     const checkPassword = bcrypt.compareSync(password, user.password);
 
-    if(!checkPassword){
+    if (!checkPassword) {
       throw new Error("password is incorrect");
     }
 
@@ -31,24 +30,28 @@ async function userSignInController(req, res) {
       const token = await jwt.sign(
         { tokenData },
         process.env.TOKEN_SECRET_KEY,
-        { expiresIn: 60 * 60 * 10 }    
+        { expiresIn: 60 * 60 * 10 }
       );
 
+      //   const tokenOption = {
+      //   httpOnly: true,              // cannot be accessed by JS
+      //   secure: process.env.NODE_ENV === "production", // only HTTPS in prod
+      //   sameSite: "strict",          // CSRF protection
+      // };
+
       const tokenOption = {
-      httpOnly: true,              // cannot be accessed by JS
-      secure: process.env.NODE_ENV === "production", // only HTTPS in prod
-      sameSite: "strict",          // CSRF protection
-    };
+        httpOnly: true, // safer, not accessible by JS
+        secure: true, // Render is HTTPS, so always true
+        sameSite: "None", // allow cross-site cookies
+      };
 
       res.cookie("token", token, tokenOption).json({
         message: "Login successfully",
         data: token,
         success: true,
-        error: false, 
+        error: false,
       });
     }
-
-
   } catch (err) {
     res.json({
       message: err.message,
